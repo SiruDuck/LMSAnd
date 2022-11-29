34761f7b54c57610adaf0bc54c2d06889e748acd
kimseoine <potato32122@gmail.com>
2022-11-29 15:42:07 +0900

    Update MyinfoFragment.java

--- LMS/app/src/main/java/com/example/lms/myinfo/MyinfoFragment.java
+++ LMS/app/src/main/java/com/example/lms/myinfo/MyinfoFragment.java
@@ -18,6 +18,11 @@ public class MyinfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_myinfo, container, false);
+        info_id = v.findViewById(R.id.info_id); info_name = v.findViewById(R.id.info_name); info_department_name = v.findViewById(R.id.info_department_name);
+        info_grade = v.findViewById(R.id.info_grade); info_state = v.findViewById(R.id.info_state   ); info_start_date = v.findViewById(R.id.info_start_date);
+        info_phone = v.findViewById(R.id.info_phone); info_email = v.findViewById(R.id.info_email); info_addr = v.findViewById(R.id.info_addr); info_post = v.findViewById(R.id.info_post);
+        start_date = v.findViewById(R.id.start_date);
+
 
 
 
