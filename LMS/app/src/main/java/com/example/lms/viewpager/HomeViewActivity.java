92e178dee28c3cf1c1bf153680ef8746b41df7d0
kimseoine <potato32122@gmail.com>
2022-12-05 16:14:25 +0900

    하는중
    하는줃ㅇ

---
+++ LMS/app/src/main/java/com/example/lms/viewpager/HomeViewActivity.java
@@ -0,0 +1,47 @@
+package com.example.lms.viewpager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.example.lms.R;
+
+public class HomeViewActivity extends AppCompatActivity {
+    public static final int NUM_PAGES =3;
+    private ViewPager2 pager;
+    private FragmentStateAdapter pagerAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_view);
+
+        pager = findViewById(R.id.carouselView);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        pager.setAdapter(pagerAdapter);
+    }
+
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter{
+        public ScreenSlidePagerAdapter(FragmentActivity fa){
+            super (fa);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {//포지션마다 있을 fragment설정
+           // if(position==0) return new ShortsFragment();
+            //else if(position==1) return new ShortsFragment2();
+            //else return new ShortFragment3();
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;  //페이지 수 지정.
+        }
+    }
+}
